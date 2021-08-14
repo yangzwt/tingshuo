ff17241767ae685964aab6e2c949754d965a857a
yangzwt <13141426337@163.com>
2021-08-14 23:58:41 +0800

    基础版本-1.父类模块新增全局依赖，2.基础业务模块新增启动及样式修改

---
+++ tingshuo-parent/README.md
@@ -0,0 +1,5 @@
+# 项目开发注意事项
+# 1. springboot和SpringCloud版本对应关系
+https://start.spring.io/actuator/info
+# 2.spring官网地址,查看springboot版本
+https://spring.io/projects/spring-boot#learn
---
+++ tingshuo-parent/tingshuo-modules/src/main/java/com/tingshuo/system/TingShuoSystemApplication.java
@@ -0,0 +1,16 @@
+package com.tingshuo.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 听说微服务，基础业务项目入口
+ *
+ * user：yangz
+ */
+@SpringBootApplication
+public class TingShuoSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TingShuoSystemApplication.class,args);
+    }
+}
