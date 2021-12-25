3c6630974a1e88d9ad2711eb6ea1ec6fc2788bbe
yangzwt <13141426337@163.com>
2021-12-25 20:56:47 +0800

    1.听说微服务，将业务模块注册到eureka注册中心中:YZ-:2021年12月25日20:56:02 add.

--- tingshuo-parent/tingshuo-modules/src/main/java/com/tingshuo/system/TingShuoSystemApplication.java
+++ tingshuo-parent/tingshuo-modules/src/main/java/com/tingshuo/system/TingShuoSystemApplication.java
@@ -3,13 +3,15 @@ package com.tingshuo.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 听说微服务，基础业务项目入口模块
- *
+ *@EnableEurekaClient 将当前项目注册到注册中心
  * user：yangz
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class TingShuoSystemApplication {
     public static void main(String[] args) {
 
