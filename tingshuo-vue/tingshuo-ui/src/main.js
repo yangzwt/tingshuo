3ea604dd978cc36fb7d90e4678acaf5a81fb5432
yangzwt <13141426337@163.com>
2023-01-02 11:41:50 +0800

    使用axios完成请求后端服务，完成用户登录并将用户信息存储到本地浏览器中

--- tingshuo-vue/tingshuo-ui/src/main.js
+++ tingshuo-vue/tingshuo-ui/src/main.js
@@ -2,11 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import ElementUI from "element-ui";
+import axios from 'axios';
 import "element-ui/lib/theme-chalk/index.css";
 import "font-awesome/css/font-awesome.min.css";
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
+
+Vue.prototype.$http=axios;
+axios.defaults.headers.post['Content-Type']='application/json;charst=UTF-8';
+//axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.baseURL="http://127.0.0.1:9080/";
 
 new Vue({
   router,
--- tingshuo-vue/tingshuo-ui/src/views/Login.vue
+++ tingshuo-vue/tingshuo-ui/src/views/Login.vue
@@ -6,14 +6,14 @@
       <!-- rules 表示添加校验的内容 -->
       <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
         <!-- prop需要校验的参数值 -->
-        <el-form-item label="用户名称" prop="username">
-          <el-input v-model="ruleForm.username" auto-complete="off" placeholder="用户名称"></el-input>
+        <el-form-item label="用户名称" prop="userId">
+          <el-input v-model="ruleForm.userId" auto-complete="off" placeholder="用户名称"></el-input>
         </el-form-item>
         <el-form-item label="用户密码" prop="password">
           <el-input type="password" v-model="ruleForm.password" auto-complete="off" placeholder="用户密码"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="login('ruleForm')">登录</el-button>
+          <el-button type="primary" @click="login()">登录</el-button>
           <el-button @click="resetForm('ruleForm')">重置</el-button>
         </el-form-item>
       </el-form>
@@ -29,31 +29,55 @@ export default {
   data() {
     return {
       ruleForm: {
-        username: "",
+        userId: "",
         password: "",
       },
       rules: {
-        username: [
+        userId: [
           { required: true, message: "请输入用户名称", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { min: 1, max: 5, message: "长度在 1 到 5 个字符", trigger: "blur" },
         ],
         password: [
           { required: true, message: "请输入用户密码", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { min: 1, max: 5, message: "长度在 1 到 5 个字符", trigger: "blur" },
         ],
       },
+      msg:''
     };
   },
   methods: {
-    login(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          console.log("error submit!!");
-          return false;
+    // login(formName) {
+    //   this.$refs[formName].validate((valid) => {
+    //     if (valid) {
+    //       alert("submit!");
+    //     } else {
+    //       console.log("error submit!!");
+    //       return false;
+    //     }
+    //   });
+    // },
+    login() {
+      let {userId,password}=this.ruleForm;
+      this.$http({
+        method:'post',
+        url:'/login',
+        data:{userId:userId,password:password}
+      }).then(res=>{//res后端接口返回的结果
+        console.log(res);
+        let {code,msg}=res.data;// eslint-disable-line no-unused-vars
+        if(code=='200'){
+          console.log(res);
+          //缓存用户到浏览器
+          sessionStorage.setItem("userId",userId);
+          //将用户基本信息存储到浏览器中
+          sessionStorage.setItem("userInfo",JSON.stringify(res.data));
+          //跳转到指定页面
+          this.$router.push('/home');
+          
+        }else{
+        alert(msg);
         }
-      });
+      })
     },
     resetForm(formName) {
       this.$refs[formName].resetFields();
@@ -62,9 +86,9 @@ export default {
 };
 </script>
 
-<style></style>
+
 <!-- 样式文件 -->
-<!-- <style lang="scss" scoped>
+<style lang="scss" scoped>
   .login-container {
     -webkit-border-radius: 5px;
     border-radius: 5px;
@@ -84,4 +108,4 @@ export default {
     .remember {
       margin: 0px 0px 35px 0px;
     }
-  } -->
+  }
